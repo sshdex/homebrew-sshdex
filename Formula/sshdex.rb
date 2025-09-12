4a42d5326ad0cbba81bf34fad05956ada44bc141
chris <chunghao777@gmail.com>
2025-09-12 15:07:55 +0800

    Update sshdex formula to version 0.1.5 with updated URLs and SHA256 checksums

--- Formula/sshdex.rb
+++ Formula/sshdex.rb
@@ -1,15 +1,15 @@
 class Sshdex < Formula
   desc "Lightweight, terminal-based SSH host manager written in Go"
   homepage "https://github.com/sshdex/sshdex"
-  version "0.1.4"
+  version "0.1.5"
   license "Apache-2.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/sshdex/sshdex/releases/download/v0.1.4/sshdex_0.1.4_darwin_arm64.tar.gz"
-    sha256 "fc1a95b71c03951a223f25e00c4386bc9a336db27c455296a70a13f0db86af37"
+    url "https://github.com/sshdex/sshdex/releases/download/v0.1.5/sshdex_0.1.5_darwin_arm64.tar.gz"
+    sha256 "95196a95addf6eaeb0e7006f49e3f9819a308c3b3f9ef9990a31773225d335a4"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/sshdex/sshdex/releases/download/v0.1.4/sshdex_0.1.4_darwin_amd64.tar.gz"
-    sha256 "49cac16261526787af2266f131bb62a17d5f5ecd52ba4852ad022a122863c1ec"
+    url "https://github.com/sshdex/sshdex/releases/download/v0.1.5/sshdex_0.1.5_darwin_amd64.tar.gz"
+    sha256 "41edc73b407cae0a4981e0c071349afcffc2523162013534c5a4eaeda6f38db0"
   else
     odie "Unsupported platform for prebuilt binary"
   end
